9c7151392aba7158e4f6a289dc236ae1a4c1fb6b
Farhan Girach <farhangirach@hotmail.com>
2025-03-22 14:23:33 +0530

    Unwanted code remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      https://www.youtube.com/watch?v=4JsTCSYst9M ---------------- 17:32 
         <UseHook/>
       </main>
     </div>
